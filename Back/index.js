dfdd85cb11e5d0b9dcf5ca4755ffdf00b407e621
Átila de Freitas <83841300+atiladefreitas@users.noreply.github.com>
2021-08-27 18:50:51 -0300

    Create index.js

---
+++ Back/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const database = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'productSystem',
+});
+
+//post requet, vai inserrir os dados no database
+app.post('/create', (req, res) => { // rota ; font end -> back end
+    //variáveis do front end
+    const name = req.body.name;
+    const num = req.body.num;
+    const desc = req.body.desc;
+    const price = req.body.price;
+    
+    database.query('INSERT INTO produtos (`name`, `num`, `desc`, `price`) VALUES (?,?,?,?)', //insert
+    [name, num, desc, price], (err, result) => { //verify
+        if (err) {
+            console.log(err);
+        } else {
+            res.send("inserido!");
+        }
+    }
+);
+});
+
+app.get('/estoque', (req, res) => {
+    database.query('SELECT * FROM produtos', (err, result) =>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const price = req.body.price;
+    database.query("UPDATE SET produtos price = ? WHERE id = ?", [price, id], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.delete('/delete/:id', (req, res) => {  //ID será acessível
+    const id = req.params.id;
+    database.query("DELETE FROM produtos WHERE id = ?", id, (err, result) => { // ? => recebe id
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    }); 
+});
+
+app.listen(3001, ()=>{
+    console.log("server on!");
+});
